61fc167a0cccfbc181babc94673a28c40a3ac427
vantung2102 <80909205+vantung2102@users.noreply.github.com>
2024-01-06 23:37:20 +0700

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,6 @@
+# exit on error
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
-bundle exec rails db:migrate
+./bin/rails assets:precompile
+./bin/rails assets:clean
